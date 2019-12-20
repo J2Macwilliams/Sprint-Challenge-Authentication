2b60d417ab55ca944be2ea544ec837001dc78bf9
Jeremy McWilliams <j2macwilliams@gmail.com>
2019-12-20 09:25:27 -0800

    Write 1st test for Login.

--- auth/authRouter.spec.js
+++ auth/authRouter.spec.js
@@ -3,3 +3,27 @@ const request = require('supertest');
 const server = require('../api/server.js');
 
 const db = require('../database/dbConfig');
+
+describe('AuthRouter', function() {
+	describe('environment', function() {
+		it('should set environment to testing', function() {
+			expect(process.env.DB_ENV).toBe('testing');
+		});
+	});
+	//LOGIN POST tests-----------------------------------
+	describe('Post /', function() {
+		beforeEach(async function() {
+			await db('users').truncate();
+		});
+		describe('add user', function() {
+			it('should return a 201 ok', function() {
+				return request(server)
+					.post('/api/login')
+					.send({ username: 'Jeremy', password: 'Bear' })
+					.then(res => {
+						expect(res.status).toBe(201);
+					});
+			});
+		});
+	});
+});
