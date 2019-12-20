908582e93df93c1c5782688dfdd7b1b37e6a393f
Jeremy McWilliams <j2macwilliams@gmail.com>
2019-12-20 08:57:03 -0800

    Require bcryptjs , signToken, and helpers into router.

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,7 +1,18 @@
 const router = require('express').Router();
 
+
+const bcrypt = require('bcryptjs');
+
+// pull in signToken
+const signToken = require('../JWT/signToken');
+
+//Pull in knex helper models
+const  userDb = require('../models/userDb');
+
 router.post('/register', (req, res) => {
   // implement registration
+
+
 });
 
 router.post('/login', (req, res) => {
