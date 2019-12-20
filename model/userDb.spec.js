5c6d4b10b39bc1c332ff8bfcc18108946bb14090
Jeremy McWilliams <j2macwilliams@gmail.com>
2019-12-20 10:55:02 -0800

    Wrote another test.

--- model/userDb.spec.js
+++ model/userDb.spec.js
@@ -1,5 +1,17 @@
-const request = require('supertest');
-
-const server = require('../api/server.js');
+const Users = require('./userDb')
 
 const db = require('../database/dbConfig');
+
+describe('Users model', function() {
+	beforeEach(async () => {
+		await db('users').truncate();
+    });
+    describe('add', function () {
+        it ('should add a user to the database', async function() {
+            await Users.add({username: 'Bear', password: 'Kitty'});
+
+            const users = await db('users');
+            expect(users).toHaveLength(1);
+        })
+    })
+});
