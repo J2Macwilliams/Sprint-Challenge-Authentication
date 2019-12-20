cd4699b993789f9f509c2894b9b7829067822e4e
Jeremy McWilliams <j2macwilliams@gmail.com>
2019-12-20 09:13:41 -0800

    Config validatePost middleware in server.js.

--- api/server.js
+++ api/server.js
@@ -2,7 +2,11 @@ const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
 
+// Pull in custom middleware
 const authenticate = require('../auth/authenticate-middleware.js');
+const validatePost = require('../middleware/validatePost.js');
+
+// Pull in Routers
 const authRouter = require('../auth/auth-router.js');
 const jokesRouter = require('../jokes/jokes-router.js');
 
@@ -12,7 +16,7 @@ server.use(helmet());
 server.use(cors());
 server.use(express.json());
 
-server.use('/api/auth', authRouter);
+server.use('/api/auth', validatePost, authRouter);
 server.use('/api/jokes', authenticate, jokesRouter);
 
 module.exports = server;
